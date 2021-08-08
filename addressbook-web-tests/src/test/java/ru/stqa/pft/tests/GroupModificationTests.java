27138835d85be8e68a82f9611899beb7e9c22c2b
YakovtsevD <nemic1976@tut.by>
2021-08-08 17:10:10 +0300

    less4.44 comparing unordered collections after modification group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -18,7 +19,8 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(2);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("newgroup6", "header6", "footer6"));
+    GroupData group = new GroupData(before.get(2).getId(),"newgroup6", "header6", "footer6");
+    app.getGroupHelper().fillGroupForm(group);
     //app.getGroupHelper().fillGroupForm(new GroupData("newgroup2", null, null));     //если не надо заполнять все значения
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
@@ -27,6 +29,11 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //Assert.assertEquals(after, before);
     Assert.assertEquals(after.size(), before.size());
+    //т.к. после модификации
+    before.remove(2);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     app.getNavigationHelper().gotoHome();
     app.getSessionHelper().logout();
   }
