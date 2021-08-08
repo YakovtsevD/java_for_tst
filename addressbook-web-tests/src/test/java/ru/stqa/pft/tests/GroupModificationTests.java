9dd8dff9bf44b1f41afad3a6e98a41f3d38f168f
YakovtsevD <nemic1976@tut.by>
2021-08-08 13:45:29 +0300

    less4.40counting the number of items on the page

--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.model.GroupData;
 
@@ -8,6 +9,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    // проверка количества групп до ввода новой
+    int before = app.getGroupHelper().getGroupCount();
+
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "header1", "footer1"));
     }
@@ -17,6 +21,10 @@ public class GroupModificationTests extends TestBase {
     //app.getGroupHelper().fillGroupForm(new GroupData("newgroup2", null, null));     //если не надо заполнять все значения
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
+    // проверка количества групп после ввода новой
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
+
     app.getNavigationHelper().gotoHome();
     app.getSessionHelper().logout();
   }
