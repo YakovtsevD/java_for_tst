10b5440fbc6f7805ec3c886d79caa2f7a9c12b11
YakovtsevD <nemic1976@tut.by>
2021-08-16 08:36:14 +0300

    less5.56.1 begining

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactPhoneTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.model.ContactData;
+
+public class ContactPhoneTests extends TestBase {
+
+    @Test
+    public void testContactPhones() {
+        app.goTo().gotoHome();
+        //ContactData contact
+    }
+
+
+}
